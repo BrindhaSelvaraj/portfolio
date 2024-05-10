350239d924167daa1200f27537873bc43aa588a8
BrindhaSelvaraj <itzmibrindha@gmail.com>
2024-05-10 13:14:11 +0530

    first commit

---
+++ src/data/ProjectsList.jsx
@@ -0,0 +1,45 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "Task Hub",
+      description:
+        "This task management app is a practical example of a full-stack MERN application, showcasing user authentication, CRUD operations, and data persistence using MongoDB. The separation of concerns between front-end and back-end technologies allows for scalable and maintainable development of modern web applications.",
+      technologies: [
+        "React JS",
+        "redux",
+        "REST APIs",
+        "axios",
+        "Html",
+        "CSS",
+        "Git",
+        "GitHub",
+        "Mongodb",
+        "Node js",
+        "Express",
+        "Bootstrap",
+      ],
+    },
+    {
+      name: "Gallary Gloom -Women dress collection UI design",
+      description:
+        "Contributed to the frontend development of Gallary gloom, a platform facilitating flexible UI design for Women dress collection. Implemented user-friendly interface features and collaborated closely.",
+      technologies: [
+        "React JS",
+        "axios",
+        "CSS",
+        "Git",
+        "GitHub",
+        "Html",
+        "Bootstrap",
+        "JavaScript"
+      ],
+    },
+    {
+      name: "Personal Portfolio",
+      description:
+        "My portfolio is a website built with React.js and Tailwind CSS, utilizing React Router v6 to enable easy access to multiple pages. It was designed and created entirely by myself, showcasing my skills and projects. The website is responsive and visually appealing, providing a user-friendly experience.",
+      technologies: ["React JS", "Tailwind CSS", "Git", "GitHub", "Figma"],
+    },
+    
+  ],
+};
---
+++ src/data/UserData.jsx
@@ -0,0 +1,35 @@
+export const UserData = {
+  name: "Brindha Selvaraj",
+  about : "I'm a front end developer that learned everything on my own. I specialize in creating dynamic, accessible websites that work on any device. Furthermore, I regularly create creative solutions that improve user experiences. I am proficient in React.js, Redux, JavaScript, HTML5, and CSS. I guarantee efficient code management and communication thanks to my knowledge with Git version control. Likewise, I'm also skilled at developing apps using mini program studio frameworks and exceeding client expectations with my high-caliber solutions.",
+  resumeUrl : "https://drive.google.com/file/d/1eb_I_UQr5aNAbX-XFCu5qtsgvmdayYJz/view?usp=drive_link",
+  FooterLink : "https://www.linkedin.com/in/brindha-s-21656a249/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/BrindhaSelvaraj",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/brindha-s-21656a249/",
+      icon: "FaLinkedinIn",
+    },
+    
+    {
+      socialMediaName: "instagram",
+      url: "https://instagram.com/brindha_selvaraj_",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "React Js Developer",
+      "Fullstack Developer",
+      "Javascript Developer",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
